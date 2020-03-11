41f84539dc777e67ff1955c61c5b8663e8442543
andyzhshg <andyzhshg@gmail.com>
2020-03-11 16:38:54 +0800

    redsocks conf update

--- ss_local.sh
+++ ss_local.sh
@@ -15,6 +15,10 @@ then
     iptables -t nat -A OUTPUT -d 172.16.0.0/16 -j RETURN
     iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
     iptables -t nat -A OUTPUT -d 127.0.0.0/8 -j RETURN
+    iptables -t nat -A OUTPUT -d 0.0.0.0/8 -j RETURN
+    iptables -t nat -A OUTPUT -d 169.254.0.0/16 -j RETURN
+    iptables -t nat -A OUTPUT -d 224.0.0.0/4 -j RETURN
+    iptables -t nat -A OUTPUT -d 240.0.0.0/4 -j RETURN
 
     # redirect all other accesses to redsocks service
     iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-ports 12345
